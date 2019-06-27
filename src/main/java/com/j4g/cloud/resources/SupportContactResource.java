cc2e284e08714220394a734cb71a6dbd69cb1fce
Edgar Ramirez Sedano <edgar@Edgars-MacBook-Pro.local>
2019-06-26 20:54:45 -0500

    Change Configuration to read from env variable

--- src/main/java/com/j4g/cloud/resources/SupportContactResource.java
+++ src/main/java/com/j4g/cloud/resources/SupportContactResource.java
@@ -1,5 +1,6 @@
 package com.j4g.cloud.resources;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +13,18 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 @RequestMapping("/support/contact")
 public class SupportContactResource {
 	
+	private String contactSupport;
+	
 	@GetMapping(produces = "application/json")
 	public ObjectNode getAPIVersion() {
 		ObjectNode version = JsonNodeFactory.instance.objectNode();
-		version.put("contactLinkedin", "https://www.linkedin.com/in/orlandocano/");
+		version.put("contactLinkedin", contactSupport);
 		return version;
 	}
+	
+	@Value("${config.support.contact}")
+	public void setContactSupport(String contactSupport) {
+		this.contactSupport = contactSupport; 
+	} 
 
 }
